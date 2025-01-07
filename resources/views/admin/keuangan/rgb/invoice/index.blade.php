f09832778eb2ff44f7363f9975ddb615ba856936
mael <ismailgilangm@gmail.com>
2025-01-07 13:51:24 +0700

    rgm

--- resources/views/admin/keuangan/rgb/invoice/index.blade.php
+++ resources/views/admin/keuangan/rgb/invoice/index.blade.php
@@ -5,7 +5,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-200 leading-tight">
-            {{ __('Invoice RGB') }}
+            {{ __('Invoice RGM') }}
         </h2>
     </x-slot>
     <div class="p-6">
